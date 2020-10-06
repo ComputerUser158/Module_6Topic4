364c20b2ceb04d338241bbf72ec94b75ad30b5f2
ComputerUser158 <oneringraw@gmail.com>
2020-10-06 05:36:13 -0500

    Topic 4: Final commit 6/6 tests passing and TypeError handled

--- test_functions/test_validate_input_in_functions.py
+++ test_functions/test_validate_input_in_functions.py
@@ -13,13 +13,14 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual("Invalid test score, try again!", val_input.score_input("Python3!", -1))
 
     def test_score_input_test_score_above_range(self):
-        self.assertEqual("Invalid test score, try again!", val_input.score_input("Python3!", 101))
+        self.assertEqual("Invalid test score, try again!", val_input.score_input("Python4!", 101))
 
     def test_test_score_non_numeric(self):
-        self.assertEqual(True, False)
+        with self.assertRaises(TypeError):
+            val_input.score_input("Python5", "fifty")
 
     def test_score_input_invalid_message(self):
-        self.assertEqual("Not an acceptable score, try again!", val_input.score_input("Python3!", 101, "Not an acceptable score, try again!"))
+        self.assertEqual("Not an acceptable score, try again!", val_input.score_input("Python6!", 101, "Not an acceptable score, try again!"))
 
 
 if __name__ == '__main__':
