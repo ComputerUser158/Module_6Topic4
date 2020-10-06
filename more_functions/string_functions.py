567cdc0207a4f18439c83dab311f832217efda06
ComputerUser158 <oneringraw@gmail.com>
2020-10-06 02:33:46 -0500

    Topic 4: first commit

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -3,9 +3,25 @@ Author: Rawley Collins
 Program: string_functions.py
 
 
-program: calls a function that takes a string message and returns it a specified number of times.
+program: contains a function that takes a string message and returns it a specified number of times.
 """
 
 
 def multiply_string(message, n):
-    pass
+    """multiplies a string.
+
+    function prints a message a certain number of times
+    only if the requested number of times is between 2 and 7
+
+    :param message: the string to be multiplied
+    :type message: str
+    :return: message
+    :rtype: str"""
+
+    if 7 > n > 2:
+        message = message * n
+        return message
+    else:
+        print("your number of iterations was not between 7 and 2")
+
+
